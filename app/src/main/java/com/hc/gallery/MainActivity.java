9696da0ad33a183c420092cde849184f9741774b
Chengzi <13340199938@163.com>
2017-05-02 09:09:55 +0800

    add AndPermission

--- app/src/main/java/com/hc/gallery/MainActivity.java
+++ app/src/main/java/com/hc/gallery/MainActivity.java
@@ -1,9 +1,11 @@
 package com.hc.gallery;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,16 +16,20 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.dhc.gallery.ui.GalleryActivity;
 import com.dhc.gallery.GalleryConfig;
 import com.dhc.gallery.GalleryHelper;
+import com.dhc.gallery.ui.GalleryActivity;
 import com.hc.gallery.util.StorageType;
 import com.hc.gallery.util.StorageUtil;
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.PermissionListener;
+import com.yanzhenjie.permission.Rationale;
+import com.yanzhenjie.permission.RationaleListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PermissionListener {
     List<String> mList;
     String outputPath;
 
@@ -31,13 +37,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StorageUtil.init(this, null);//初始化监测sdk
+        StorageUtil.init(this, null);//初始化监测sdcard
         mList = new ArrayList<>();
         mList.add((mList.size() + 1) + ".  选择单张图片");
         mList.add((mList.size() + 1) + ".  选择单张图片并裁剪");
         mList.add((mList.size() + 1) + ".  选择多张图片");
         mList.add((mList.size() + 1) + ".  选择视频");
-        mList.add((mList.size() + 1) + ".  拍摄视频");
+        mList.add((mList.size() + 1) + ".  拍摄视频(可限制时长)");
         mList.add((mList.size() + 1) + ".  拍照片");
         mList.add((mList.size() + 1) + ".  拍照片并裁剪");
         ListView listView = (ListView) findViewById(R.id.ls_home);
@@ -47,6 +53,21 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(!AndPermission.hasPermission(MainActivity.this
+                        , Manifest.permission.CAMERA
+                        , Manifest.permission.READ_PHONE_STATE
+                        , Manifest.permission.RECORD_AUDIO)) {
+                    AndPermission.with(MainActivity.this)
+                            .requestCode(100)
+                            .permission(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO,Manifest.permission.READ_PHONE_STATE)
+                            .rationale(new RationaleListener() {
+                                @Override
+                                public void showRequestPermissionRationale(int requestCode, Rationale rationale) {
+                                    AndPermission.rationaleDialog(MainActivity.this, rationale).show();
+                                }
+                            })
+                             .send();
+                }
                 switch (position) {
                     case 0: /*** 选择单张图片 onActivityResult{@link GalleryActivity.PHOTOS}*/
                         GalleryHelper.with(MainActivity.this).type(GalleryConfig.SELECT_PHOTO).requestCode(12).singlePhoto().execute();
@@ -62,7 +83,10 @@ public class MainActivity extends AppCompatActivity {
                         GalleryHelper.with(MainActivity.this).type(GalleryConfig.SELECT_VEDIO).requestCode(12).isSingleVedio().execute();
                         break;
                     case 4:/***拍摄视频 onActivityResult{@link GalleryActivity.VIDEO}*/
-                        GalleryHelper.with(MainActivity.this).type(GalleryConfig.RECORD_VEDIO).requestCode(12).execute();
+
+                        GalleryHelper.with(MainActivity.this).type(GalleryConfig.RECORD_VEDIO).requestCode(12)
+//                                .limitRecordTime(10)//限制时长
+                                .execute();
                         break;
                     case 5:/***拍照片onActivityResult {@link GalleryActivity.PHOTOS}*/
                         GalleryHelper.with(MainActivity.this).type(GalleryConfig.TAKE_PHOTO).requestCode(12).execute();
@@ -77,6 +101,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     private class MyAdapter extends BaseAdapter {
         Context mContext;
@@ -124,6 +150,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //----------------------------------权限回调处理----------------------------------//
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
+            grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        /**
+         * 转给AndPermission分析结果。
+         *
+         * @param requestCode  请求码。
+         * @param permissions  权限数组，一个或者多个。
+         * @param grantResults 请求结果。
+         * @param listener PermissionListener 对象。
+         */
+        AndPermission.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+
+    @Override
+    public void onSucceed(int requestCode, List<String> grantPermissions) {
+        Toast.makeText(this, "权限获取成功", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onFailed(int requestCode, List<String> deniedPermissions) {
+        Toast.makeText(this, "权限获取失败", Toast.LENGTH_SHORT).show();
+        // 用户否勾选了不再提示并且拒绝了权限，那么提示用户到设置中授权。
+        if (AndPermission.hasAlwaysDeniedPermission(this, deniedPermissions)) {
+            // 第一种：用默认的提示语。
+            AndPermission.defaultSettingDialog(this, 300).show();
+        }
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -145,6 +205,8 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             }
+        }else if(requestCode==300&&resultCode == Activity.RESULT_OK){
+                Toast.makeText(this, "从设置回来", Toast.LENGTH_LONG).show();
         }
 
     }
